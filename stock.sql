3caefd62386db43cd76cd045121e2907de0135f9
Mihirsinh <vajamihir20@gmail.com>
2021-02-24 16:25:09 +0530

    Initial Commit

---
+++ stock.sql
@@ -0,0 +1,39 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE `login` (
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `balance` double(255,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `portfolio` (
+  `username` varchar(255) NOT NULL,
+  `s_sname` varchar(255) NOT NULL,
+  `s_name` varchar(255) NOT NULL,
+  `s_quantity` int(255) NOT NULL,
+  `s_totalprice` double(255,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `transaction` (
+  `username` varchar(255) NOT NULL,
+  `s_sname` varchar(255) NOT NULL,
+  `s_name` varchar(255) NOT NULL,
+  `date` timestamp NULL DEFAULT current_timestamp(),
+  `buy` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`username`);
+
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`username`);
+
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`username`);
