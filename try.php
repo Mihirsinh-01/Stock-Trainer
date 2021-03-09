eff76ade074a3c510a391828d95bf2904847c1fa
Mihirsinh <vajamihir20@gmail.com>
2021-03-09 19:09:00 +0530

    Initial Commit

--- try.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    echo "<script>
-            var javascriptVar = 'success';
-         </script>";
-    
-
-    $phpVar = "<script>document.writeln(javascriptVar);</script>";
-    echo $phpVar;
-?>
